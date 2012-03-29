75c05590b68eaa68489c5afe310bb80f7d2402cb
Brandyn A. White <bwhite@dappervision.com>
2012-03-29 00:47:33 -0400

    Updated
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- image_search/search_database.py
+++ image_search/search_database.py
@@ -1,23 +1,20 @@
 from compute_hashes import compute_hash
 from build_features import compute_feature
-import imfeat
-import distpy
-import hadoopy
 import numpy as np
 import cv2
 import cPickle as pickle
+import image_search
 
 
-def evaluate_image(image, hashes, image_metadatas, median_feature, k=50, max_tags=10):
+def evaluate_image(image, db, image_metadatas, median_feature, k=50, max_tags=10):
     feature = compute_feature(image)
     feature = feature.reshape((1, feature.size))
     h = compute_hash(feature, median_feature).ravel()
     # Compute K-nn
-    d = distpy.Hamming(len(h))
     tags = []
     with open('results.html', 'w') as fp:
         fp.write('<img height=250 src=%s \><br>' % 'target.jpg')
-        for distance, index in d.knn(hashes, h, k):  # (distance, index) (k x 2)
+        for index in db.search(h, k=k):
             tags += image_metadatas[index]['tags']
             fp.write('<img height=150 src=%s \>' % image_metadatas[index]['url_m'])
     # Pool tags
@@ -34,12 +31,13 @@ def main():
     with open('database.pkl') as fp:
         hashes, image_metadatas, median_feature = pickle.load(fp)
     hashes = np.array([x.ravel() for x in hashes])
+    db = image_search.db.Linear(hashes.shape[1])
+    db.store(hashes, np.array(np.arange(hashes.shape[0]), dtype=np.uint64))
     for x in image_metadatas:
         x['tags'] = x['tags'].split()
-    print(hashes.shape)
     print('Model loaded')
     # load test image
     image = cv2.imread('target.jpg')
-    print(evaluate_image(image, hashes, image_metadatas, median_feature))
+    print(evaluate_image(image, db, image_metadatas, median_feature))
 
 main()
