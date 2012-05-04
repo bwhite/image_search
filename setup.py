8658ad560ad1d8c99d3132e4137bf364ef464f85
Brandyn A. White <bwhite@dappervision.com>
2012-05-04 14:43:22 -0400

    Setup
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='image_search',
+      version='.01',
+      author='Brandyn A. White',
+      author_email='bwhite@dappervision.com',
+      license='GPLv3',
+      packages=find_packages())
